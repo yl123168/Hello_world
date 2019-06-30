b167f125b83a23f8732393539f6ea3d8b2e4c2d0
yl123168 <724162642@qq.com>
2019-06-30 16:08:50 +0800

    Create 02学生分数.py

---
+++ week4/Lecture8/02学生分数.py
@@ -0,0 +1,22 @@
+def get_stats(subject, weights):
+    return [[elt[0], elt[1], avg(elt[1], weights)]for elt in subject]
+
+def dot_product(a,b):
+    result = 0.0
+    for i in range(len(a)):
+        result += a[i]*b[i]
+    return result
+
+def avg(grades, weights):
+    return dot_product(grades, weights) / len(grades)
+
+test = [
+   [ ['fred','flintstone'],[10.0, 5.0, 85.0]],
+   [['barney','rubble'], [10.0, 8.0, 74.0]],
+   [['wilma', 'flintatone'], [8.0, 10.0, 96.0]],
+   [['dino'], []]
+   ]
+
+weights = [.3, .2, .5]
+
+print(get_stats(test, weights))
