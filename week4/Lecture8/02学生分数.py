68e6e643af9c531e1ef944b6b4202616e7a345de
yl123168 <724162642@qq.com>
2019-06-30 16:43:18 +0800

    Update 02学生分数.py

--- week4/Lecture8/02学生分数.py
+++ week4/Lecture8/02学生分数.py
@@ -2,10 +2,16 @@ def get_stats(subject, weights):
     return [[elt[0], elt[1], avg(elt[1], weights)]for elt in subject]
 
 def dot_product(a,b):
-    result = 0.0
-    for i in range(len(a)):
-        result += a[i]*b[i]
-    return result
+
+        result = 0.0
+        for i in range(len(a)):
+            try:
+                result += a[i]*b[i]
+            except TypeError:
+                result += convert(a[i]) * b[i]
+        return result
+   
+    
 
 def avg(grades, weights):
     try:
@@ -13,9 +19,24 @@ def avg(grades, weights):
     except ZeroDivisionError:
         print "divide by zero Error"
         return 0.0
+    
+def convert(grades):
+    if type(grades) == int:
+        return grades
+    elif grades == 'a':
+        return 90.0
+    elif grades == 'b':
+        return 80.0
+    elif grades == 'c':
+        return 70.0
+    elif grades == 'd':
+        return 60.0
+    else:
+        return 50.0
+    
 
 test = [
-   [ ['fred','flintstone'],[10.0, 5.0, 85.0]],
+   [ ['fred','flintstone'],[10.0, 5.0, 'a']],
    [['barney','rubble'], [10.0, 8.0, 74.0]],
    [['wilma', 'flintatone'], [8.0, 10.0, 96.0]],
    [['dino'], []]
